c97b63031be03fff4c755a7d49bc3115ada6ca54
ryo <>
2023-03-31 22:44:43 +0900

    途中までコミット（three.js関連のライブラリをnpm installしたからたくさんファイルある。）

--- 3d-model-website/src/App.tsx
+++ 3d-model-website/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import * as THREE from "three";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let canvas: HTMLCanvasElement | null;
+  useEffect(() => {
+    canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
+    const sizes = {
+      width: innerWidth,
+      height: innerHeight,
+    };
+    //scene
+    const scene: THREE.Scene = new THREE.Scene();
+
+    // camera
+    const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+
+    // renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+  }, []);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <canvas id="canvas"></canvas>
+      <div className="mainContent"></div>
+    </>
+  );
 }
 
-export default App
+export default App;
